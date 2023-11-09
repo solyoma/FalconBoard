55427d137df322f7527c91c8218e42f6c4b2ae6d
András Sólyom <37068759+solyoma@users.noreply.github.com>
2023-11-09 20:02:38 +0100

    bugfix: movement keys with rubberBand did not work

--- FalconBoard/DrawArea.cpp
+++ FalconBoard/DrawArea.cpp
@@ -884,7 +884,7 @@ void DrawArea::keyPressEvent(QKeyEvent* event)
 				_Redraw();
 			}
 		}
-		else if (bMovementKeys && !_rubberBand)
+		else if (bMovementKeys && (!_rubberBand || _rubberBand && !_mods.testFlag(Qt::ControlModifier)) )
 		{
 			int stepSize = _mods.testFlag(Qt::ControlModifier) ? LargeStep : (_mods.testFlag(Qt::ShiftModifier) ? SmallStep : NormalStep);
 			if (key == Qt::Key_PageUp)
